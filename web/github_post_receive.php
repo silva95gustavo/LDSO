54ffc5966107722560730722d10403d77f8a5e8e
Gustavo Silva <silva95gustavo@gmail.com>
2016-10-17 15:46:18 +0000

    Fixed github post receive script

--- web/github_post_receive.php
+++ web/github_post_receive.php
@@ -1,6 +1,8 @@
 <?php
 // Use in the "Post-Receive URLs" section of your GitHub repo.
-if ( $_POST['payload'] && $_POST['payload']['ref'] == "refs/head/master" ) {
-  shell_exec( 'cd /var/www/html/ && git reset --hard HEAD && git pull' );
+if ( $_POST['payload']) {
+  $payload = json_decode($_POST['payload']);
+  if ($payload->ref == "refs/heads/master")
+    shell_exec( 'cd /var/www/repo/LDSO/ && git reset --hard HEAD && git pull' );
 }
 ?>
