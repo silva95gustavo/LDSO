7c80b008baddfaabb394bf01214c566628265d30
Gustavo Silva <silva95gustavo@gmail.com>
2016-10-17 02:37:27 +0000

    Live server

---
+++ web/sites/default/files/php/twig/3ef7b39e_filter-guidelines.html.twig_eb36e89b62235c341bf2f799c81578f49462d6ad08135aba731334c1bbb72f0c/aaa71813bf93da1017bf508201014e1615105d6a4e8cec131e38f23a7e3de2c0.php
@@ -0,0 +1,80 @@
+<?php
+
+/* core/themes/classy/templates/content-edit/filter-guidelines.html.twig */
+class __TwigTemplate_19520ec78c5439997a94fc7b394d16340c033881c65dc50b8824d4c45f8a7609 extends Twig_Template
+{
+    public function __construct(Twig_Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->parent = false;
+
+        $this->blocks = array(
+        );
+    }
+
+    protected function doDisplay(array $context, array $blocks = array())
+    {
+        $tags = array("set" => 21);
+        $filters = array();
+        $functions = array();
+
+        try {
+            $this->env->getExtension('Twig_Extension_Sandbox')->checkSecurity(
+                array('set'),
+                array(),
+                array()
+            );
+        } catch (Twig_Sandbox_SecurityError $e) {
+            $e->setTemplateFile($this->getTemplateName());
+
+            if ($e instanceof Twig_Sandbox_SecurityNotAllowedTagError && isset($tags[$e->getTagName()])) {
+                $e->setTemplateLine($tags[$e->getTagName()]);
+            } elseif ($e instanceof Twig_Sandbox_SecurityNotAllowedFilterError && isset($filters[$e->getFilterName()])) {
+                $e->setTemplateLine($filters[$e->getFilterName()]);
+            } elseif ($e instanceof Twig_Sandbox_SecurityNotAllowedFunctionError && isset($functions[$e->getFunctionName()])) {
+                $e->setTemplateLine($functions[$e->getFunctionName()]);
+            }
+
+            throw $e;
+        }
+
+        // line 21
+        $context["classes"] = array(0 => "filter-guidelines-item", 1 => ("filter-guidelines-" . $this->getAttribute(        // line 23
+(isset($context["format"]) ? $context["format"] : null), "id", array())));
+        // line 26
+        echo "<div";
+        echo $this->env->getExtension('Twig_Extension_Sandbox')->ensureToStringAllowed($this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, $this->getAttribute((isset($context["attributes"]) ? $context["attributes"] : null), "addClass", array(0 => (isset($context["classes"]) ? $context["classes"] : null)), "method"), "html", null, true));
+        echo ">
+  <h4 class=\"label\">";
+        // line 27
+        echo $this->env->getExtension('Twig_Extension_Sandbox')->ensureToStringAllowed($this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, $this->getAttribute((isset($context["format"]) ? $context["format"] : null), "label", array()), "html", null, true));
+        echo "</h4>
+  ";
+        // line 28
+        echo $this->env->getExtension('Twig_Extension_Sandbox')->ensureToStringAllowed($this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, (isset($context["tips"]) ? $context["tips"] : null), "html", null, true));
+        echo "
+</div>
+";
+    }
+
+    public function getTemplateName()
+    {
+        return "core/themes/classy/templates/content-edit/filter-guidelines.html.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  55 => 28,  51 => 27,  46 => 26,  44 => 23,  43 => 21,);
+    }
+
+    public function getSource()
+    {
+        return "";
+    }
+}
---
+++ web/sites/default/files/php/twig/3ef7b39e_user.html.twig_c0971bce28c3aa030cf127d2e381157ac35cf4c55b36755a78a98c925a9d8145/02fd298b6a32f09e160a87de2f617be085afa3ed071fae56a369ce251d59b02c.php
@@ -0,0 +1,76 @@
+<?php
+
+/* core/themes/classy/templates/user/user.html.twig */
+class __TwigTemplate_f88ec5b014297c972f50b0e43013a15317c16f2e93bb1ed1f05869e6388a14b4 extends Twig_Template
+{
+    public function __construct(Twig_Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->parent = false;
+
+        $this->blocks = array(
+        );
+    }
+
+    protected function doDisplay(array $context, array $blocks = array())
+    {
+        $tags = array("if" => 20);
+        $filters = array();
+        $functions = array();
+
+        try {
+            $this->env->getExtension('Twig_Extension_Sandbox')->checkSecurity(
+                array('if'),
+                array(),
+                array()
+            );
+        } catch (Twig_Sandbox_SecurityError $e) {
+            $e->setTemplateFile($this->getTemplateName());
+
+            if ($e instanceof Twig_Sandbox_SecurityNotAllowedTagError && isset($tags[$e->getTagName()])) {
+                $e->setTemplateLine($tags[$e->getTagName()]);
+            } elseif ($e instanceof Twig_Sandbox_SecurityNotAllowedFilterError && isset($filters[$e->getFilterName()])) {
+                $e->setTemplateLine($filters[$e->getFilterName()]);
+            } elseif ($e instanceof Twig_Sandbox_SecurityNotAllowedFunctionError && isset($functions[$e->getFunctionName()])) {
+                $e->setTemplateLine($functions[$e->getFunctionName()]);
+            }
+
+            throw $e;
+        }
+
+        // line 19
+        echo "<article";
+        echo $this->env->getExtension('Twig_Extension_Sandbox')->ensureToStringAllowed($this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, $this->getAttribute((isset($context["attributes"]) ? $context["attributes"] : null), "addClass", array(0 => "profile"), "method"), "html", null, true));
+        echo ">
+  ";
+        // line 20
+        if ((isset($context["content"]) ? $context["content"] : null)) {
+            // line 21
+            echo $this->env->getExtension('Twig_Extension_Sandbox')->ensureToStringAllowed($this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, (isset($context["content"]) ? $context["content"] : null), "html", null, true));
+        }
+        // line 23
+        echo "</article>
+";
+    }
+
+    public function getTemplateName()
+    {
+        return "core/themes/classy/templates/user/user.html.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  53 => 23,  50 => 21,  48 => 20,  43 => 19,);
+    }
+
+    public function getSource()
+    {
+        return "";
+    }
+}
