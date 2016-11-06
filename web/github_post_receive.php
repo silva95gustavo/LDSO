97e0c0682e83ff24ce83af728ee44fe6b960f83a
Gustavo Silva <silva95gustavo@gmail.com>
2016-11-06 23:40:47 +0000

    Update github_post_receive.php

--- web/github_post_receive.php
+++ web/github_post_receive.php
@@ -2,7 +2,9 @@
 // Use in the "Post-Receive URLs" section of your GitHub repo.
 if ( $_POST['payload']) {
   $payload = json_decode($_POST['payload']);
-  if ($payload->ref == "refs/heads/master" || $payload->ref == "refs/heads/develop")
+  if ($payload->ref == "refs/heads/master")
     shell_exec( 'cd /var/www/repo/LDSO/ && git reset --hard HEAD && git pull' );
+  else if ($payload->ref == "refs/heads/develop")
+    shell_exec( 'cd /var/www/devrepo/LDSO/ && git reset --hard HEAD && git pull' );
 }
 ?>
