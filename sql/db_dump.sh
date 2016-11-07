9745997c659ce9a4d53529373fdf7b2f4c6ad13d
Gustavo Silva <silva95gustavo@gmail.com>
2016-11-07 01:09:28 +0000

    Update db_dump.sh

--- sql/db_dump.sh
+++ sql/db_dump.sh
@@ -1,2 +1,8 @@
-mysqldump --no-data --add-drop-table cuidadores > cuidadores.sql
-mysqldump --no-create-info --ignore-table=cuidadores.cache_bootstrap --ignore-table=cuidadores.cache_config --ignore-table=cuidadores.cache_container --ignore-table=cuidadores.cache_data --ignore-table=cuidadores.cache_default --ignore-table=cuidadores.cache_discovery --ignore-table=cuidadores.cache_dynamic_page_cache --ignore-table=cuidadores.cache_entity --ignore-table=cuidadores.cache_menu --ignore-table=cuidadores.cache_render --ignore-table=cuidadores.cache_toolbar --ignore-table=cuidadores.session --ignore-table=cuidadores.watchdog cuidadores >> cuidadores.sql
+#!/bin/bash
+
+if [ "$#" == "0" ]; then
+    echo "Usage: $0 database"
+fi
+
+mysqldump --no-data --add-drop-table $1 > cuidadores.sql
+mysqldump --no-create-info --ignore-table=$1.cache_bootstrap --ignore-table=$1.cache_config --ignore-table=$1.cache_container --ignore-table=$1.cache_data --ignore-table=$1.cache_default --ignore-table=$1.cache_discovery --ignore-table=$1.cache_dynamic_page_cache --ignore-table=$1.cache_entity --ignore-table=$1.cache_menu --ignore-table=$1.cache_render --ignore-table=$1.cache_toolbar --ignore-table=$1.session --ignore-table=$1.watchdog $1 >> cuidadores.sql
