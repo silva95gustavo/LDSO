47a0e25a473ac91746e9da12fab1ecd19dc7c763
F0lha <pedrovieiracastro@hotmail.com>
2016-12-03 16:15:10 +0000

    mini fix

--- web/modules/exportinfo/src/exportinfo.php
+++ web/modules/exportinfo/src/exportinfo.php
@@ -18,21 +18,21 @@
 
 				$dbh = Database::getConnection();
 	  	
-	  			$stmt = db_query("	SELECT 1 as type, cuidadores_users.email as email, community_users_groups.group_id as faixa_etaria, community_users.username as username, community_users.is_activated as activada 
-	  										FROM cuidadores_users, community_users_groups, community_users
-	  										WHERE cuidadores_users.id_community = community_users.id AND community_users_groups.user_id = cuidadores_users.id_community
-	  									UNION 
-	  									SELECT 2 as type, cuidadores_users.email as email, NULL as faixa_etaria, NULL as username, NULL as activada
-	  										FROM cuidadores_users, community_users_groups, community_users
-	  										WHERE cuidadores_users.id_community IS NULL 
-	  									UNION
-	  									SELECT 3 as type, community_users.email as email, community_users_groups.group_id as faixa_etaria, community_users.username as username, community_users.is_activated as activada 
-	  										FROM cuidadores_users, community_users_groups, community_users
-	  										WHERE community_users.email NOT IN (SELECT email FROM cuidadores_users) AND community_users_groups.user_id = community_users.id
-	  									");
+	  			$stmt = db_query("	SELECT 1 as type, cuidadores_users.associate_nr as associate_nr, cuidadores_users.name as name, cuidadores_users.email as email, community_users_groups.group_id as faixa_etaria, community_users.username as username, community_users.is_activated as activada 
+										FROM cuidadores_users, community_users_groups, community_users
+										WHERE cuidadores_users.id_community = community_users.id AND community_users_groups.user_id = cuidadores_users.id_community
+									UNION 
+									SELECT 2 as type, cuidadores_users.associate_nr as associate_nr, cuidadores_users.name as name, cuidadores_users.email as email, NULL as faixa_etaria, NULL as username, NULL as activada
+										FROM cuidadores_users, community_users_groups, community_users
+										WHERE cuidadores_users.id_community IS NULL 
+									UNION
+									SELECT 3 as type, NULL as associate_nr, NULL as name,community_users.email as email, community_users_groups.group_id as faixa_etaria, community_users.username as username, community_users.is_activated as activada 
+										FROM cuidadores_users, community_users_groups, community_users
+										WHERE community_users.email NOT IN (SELECT email FROM cuidadores_users) AND community_users_groups.user_id = community_users.id
+									");
 	  				$string;
 	  				$string = $string . "<table>"; // start a table tag in the HTML
-	  				$string = $string . "<tr><th>Tipo de Conta</th><th>Email</th><th>Username</th><th>Faixa Etaria</th><th>Conta Activada</th></tr>";
+	  				$string = $string . "<tr><th>Tipo de Conta</th><th>Email</th><th>Nome</th><th>Nr Associado</th><th>Username</th><th>Faixa Etaria</th><th>Conta Activada</th></tr>";
 	  	
 	  				while ($row = $stmt->fetchAssoc()){  
 	  					$string = $string . "<tr>";
@@ -54,11 +54,13 @@
 	  					$string = $string . "</td><td>";
 	  					$string = $string . $row['email'] . "</td><td>";
 						if($row['name'])
-							echo $row['name'];
-						else echo "Nao Disponivel";
+							$string = $string . $row['name'];
+						else $string = $string . "Nao Disponivel";
+	  					$string = $string . "</td><td>";
 						if($row['associate_nr'])
-							echo $row['associate_nr'];
-						else echo "Nao Disponivel";
+							$string = $string . $row['associate_nr'];
+						else $string = $string . "Nao Disponivel";
+	  					$string = $string . "</td><td>";
 	  					if($row['username'])
 	  						$string = $string . $row['username'];
 	  					else $string = $string . "Nao Disponivel";
