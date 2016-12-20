dc6590b4195eb9eb7080cd018e87d0c6f12ee2ae
José Rebelo <soaresrebelo@gmail.com>
2016-12-20 15:11:37 +0000

    Remove cuidadores_users from db_dump

--- sql/db_dump.sh
+++ sql/db_dump.sh
@@ -5,4 +5,4 @@ if [ "$#" == "0" ]; then
 fi
 
 mysqldump --no-data --add-drop-table $1 > cuidadores.sql
-mysqldump --no-create-info --skip-triggers --ignore-table=$1.cache_bootstrap --ignore-table=$1.cache_config --ignore-table=$1.cache_container --ignore-table=$1.cache_data --ignore-table=$1.cache_default --ignore-table=$1.cache_discovery --ignore-table=$1.cache_dynamic_page_cache --ignore-table=$1.cache_entity --ignore-table=$1.cache_menu --ignore-table=$1.cache_render --ignore-table=$1.cache_toolbar --ignore-table=$1.session --ignore-table=$1.watchdog $1 >> cuidadores.sql
+mysqldump --no-create-info --skip-triggers --ignore-table=$1.cache_bootstrap --ignore-table=$1.cache_config --ignore-table=$1.cache_container --ignore-table=$1.cache_data --ignore-table=$1.cache_default --ignore-table=$1.cache_discovery --ignore-table=$1.cache_dynamic_page_cache --ignore-table=$1.cache_entity --ignore-table=$1.cache_menu --ignore-table=$1.cache_render --ignore-table=$1.cache_toolbar --ignore-table=$1.session --ignore-table=$1.watchdog --ignore-table=$1.cuidadores_users $1 >> cuidadores.sql
